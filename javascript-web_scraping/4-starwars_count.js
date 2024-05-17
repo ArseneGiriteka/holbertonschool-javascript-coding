63bdf17293b5ff370a91f052c0992d9ddbbda277
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-05-17 09:09:12 +0200

    fixing

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const character = 'https://swapi-api.hbtn.io/api/people/18/';
 const url = process.argv[2];
+const characterId = '18';
 
 request(url, function (error, response, body) {
   if (!error) {
     let count = 0;
     const movies = JSON.parse(body).results;
     for (const movie of movies) {
-      for (const cara of movie.characters) {
-        if (cara === character) {
+      for (const character of movie.characters) {
+        if (character.includes(characterId)) {
           count++;
         }
       }

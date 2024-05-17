d6445908248489c6de8582b725f4dd6adb13ddd3
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-05-17 08:23:24 +0200

    fix

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,7 +4,7 @@ const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.log('');
   } else {
     console.log('code: ', response.statusCode);
   }

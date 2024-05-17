5847cc259d373a925885979936a69f7974b77b8d
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-05-17 07:57:34 +0200

    fixing

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 try {
-    const data = fs.readFileSync(filePath, 'utf-8');
-    console.log(data);
-}
-catch(err){
-    console.log(err);
+  const data = fs.readFileSync(filePath, 'utf-8');
+  console.log(data);
+} catch (err) {
+  console.log(err);
 }

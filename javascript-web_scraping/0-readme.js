524ea72266698d7f7facd15d8c6e187fdd19d6a1
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-05-17 07:47:32 +0200

    task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.argv[2];
+
+try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    console.log(data);
+}
+catch(err){
+    console.log(err);
+}
